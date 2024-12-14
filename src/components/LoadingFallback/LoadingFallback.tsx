0edb3a791646e7b5d09e00b9bbfbe5bf1f5c9a14
Piotr Lechowicz <ptrlechowicz@gmail.com>
2024-12-14 19:35:01 +0100

    created a LoadingFallback component

---
+++ src/components/LoadingFallback/LoadingFallback.tsx
@@ -0,0 +1,5 @@
+export const LoadingFallback = () => (
+	<p className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-2xl text-primary-800 dark:text-primary-200'>
+		Loading...
+	</p>
+);
